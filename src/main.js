4cbd43980feabb244494675f804b339a03450ea0
ehmicky <ehmicky@gmail.com>
2019-08-08 10:00:00 +0200

    Fix main entry point

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { fetchUrl } from './fetch.js'
 
 // Fetch all available Node versions by making a HTTP request to Node website
 // Versions are already sorted from newest to oldest
-const fetchVersions = async function() {
+const allNodeVersions = async function() {
   const response = await fetchUrl(INDEX_URL)
   const index = await response.json()
   const versions = index.map(getVersionField)
@@ -15,3 +15,7 @@ const getVersionField = function({ version }) {
   // Remove the leading `v`
   return version.slice(1)
 }
+
+// We do not use `export default` because Babel transpiles it in a way that
+// requires CommonJS users to `require(...).default` instead of `require(...)`.
+module.exports = allNodeVersions
