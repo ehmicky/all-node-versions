6529b8e9d990b565c13771bb7a00dec62049c65a
ehmicky <ehmicky@gmail.com>
2019-08-09 10:00:00 +0200

    Add test

--- test/main.js
+++ test/main.js
@@ -1,5 +1,15 @@
 import test from 'ava'
 
-test('Dummy test', t => {
-  t.pass()
+import allNodeVersions from '../src/main.js'
+
+test('Success', async t => {
+  const versions = await allNodeVersions()
+  t.true(Array.isArray(versions))
+  t.true(versions.every(isVersion))
 })
+
+const isVersion = function(version) {
+  return typeof version === 'string' && VERSION_REGEXP.test(version)
+}
+
+const VERSION_REGEXP = /^\d+\.\d+\.\d+$/u
