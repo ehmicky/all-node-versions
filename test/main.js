c05a5b3184315c4f90f997fea5f2a20fe79d2692
ehmicky <ehmicky@gmail.com>
2020-04-12 17:20:48 +0200

    Add tests

--- test/main.js
+++ test/main.js
@@ -1,14 +1,8 @@
 import test from 'ava'
+import isPlainObj from 'is-plain-obj'
 import { rcompare } from 'semver'
 
 import allNodeVersions from '../src/main.js'
-
-test('Success', async (t) => {
-  const { versions } = await allNodeVersions({ fetch: true })
-
-  t.true(Array.isArray(versions))
-  t.true(versions.every(isVersion))
-})
 
 const isVersion = function (version) {
   return typeof version === 'string' && VERSION_REGEXP.test(version)
@@ -16,10 +10,74 @@ const isVersion = function (version) {
 
 const VERSION_REGEXP = /^\d+\.\d+\.\d+$/u
 
-test('Versions are sorted', async (t) => {
+test('"versions" are present', async (t) => {
+  const { versions } = await allNodeVersions({ fetch: true })
+
+  t.true(Array.isArray(versions))
+  t.true(versions.every(isVersion))
+})
+
+test('"versions" are sorted', async (t) => {
   const { versions } = await allNodeVersions({ fetch: true })
   // eslint-disable-next-line fp/no-mutating-methods
   const sortedVersions = versions.slice().sort(rcompare)
 
-  t.true(versions.every((version, index) => version === sortedVersions[index]))
+  t.deepEqual(versions, sortedVersions)
 })
+
+test('"majors" are present', async (t) => {
+  const { majors } = await allNodeVersions({ fetch: true })
+
+  t.true(Array.isArray(majors))
+  t.true(majors.every(isPlainObj))
+})
+
+test('"majors.major" are present', async (t) => {
+  const { majors } = await allNodeVersions({ fetch: true })
+
+  t.true(majors.every(isValidMajor))
+})
+
+const isValidMajor = function ({ major }) {
+  return Number.isInteger(major)
+}
+
+test('"majors.major" are sorted', async (t) => {
+  const { majors } = await allNodeVersions({ fetch: true })
+  // eslint-disable-next-line fp/no-mutating-methods
+  const sortedMajors = majors.slice().sort(compareMajor)
+
+  t.deepEqual(majors, sortedMajors)
+})
+
+const compareMajor = function ({ major: majorA }, { major: majorB }) {
+  return majorA < majorB ? 1 : -1
+}
+
+test('"majors.latest" are present', async (t) => {
+  const { majors } = await allNodeVersions({ fetch: true })
+
+  t.true(majors.every(isValidLatest))
+})
+
+const isValidLatest = function ({ latest }) {
+  return isVersion(latest)
+}
+
+test('"majors.lts" are present', async (t) => {
+  const { majors } = await allNodeVersions({ fetch: true })
+
+  t.true(majors.every(isValidLts))
+  t.false(majors.every(isLts))
+})
+
+const isValidLts = function ({ lts }) {
+  return (
+    lts === undefined ||
+    (typeof lts === 'string' && lts !== '' && lts.toLowerCase() === lts)
+  )
+}
+
+const isLts = function ({ lts }) {
+  return lts !== undefined
+}
