a650d8ba51134ab76e19e18715ce2e14530bf997
ehmicky <ehmicky@gmail.com>
2020-04-12 15:09:38 +0200

    Fix test

--- test/options.js
+++ test/options.js
@@ -4,7 +4,7 @@ import { each } from 'test-each'
 import allNodeVersions from '../src/main.js'
 
 each(
-  [{ mirror: 'not_valid_url', fetch: true }, { fetch: null }],
+  [{ mirror: 'not_valid_url', fetch: true }, { fetch: 0 }],
   ({ title }, opts) => {
     test(`Invalid options | ${title}`, async (t) => {
       await t.throwsAsync(allNodeVersions(opts))
