3481df061650cb03958aad6b3ac7f71483348559
ehmicky <ehmicky@gmail.com>
2020-05-03 11:19:20 +0200

    Refactoring

--- src/fetch.js
+++ src/fetch.js
@@ -1,8 +1,38 @@
+import { env } from 'process'
+
+import getCacheDir from 'cachedir'
 import fetchNodeWebsite from 'fetch-node-website'
 
+import { moizeFs } from './cache/moize.js'
+
 // Do the actual HTTP request
-export const fetchIndex = function ({ fetchNodeOpts }) {
+const mFetchIndex = function ({ fetchNodeOpts }) {
   return fetchNodeWebsite(INDEX_PATH, { ...fetchNodeOpts, progress: false })
 }
 
 const INDEX_PATH = 'index.json'
+
+// The cache is persisted to `GLOBAL_CACHE_DIR/nve/versions.json`.
+const getCachePath = function () {
+  const cacheDir = getCacheDir(CACHE_DIR)
+  const cacheFilename = env.TEST_CACHE_FILENAME || CACHE_FILENAME
+  return `${cacheDir}/${cacheFilename}`
+}
+
+const CACHE_DIR = 'nve'
+const CACHE_FILENAME = 'node_versions.json'
+
+// One hour
+const MAX_AGE_MS = 36e5
+
+// Cache the return value on the filesystem.
+// It has a TTL of one hour.
+// If the `fetch` option is `true`, we do not read the cache (but still write
+// it)
+export const fetchIndex = moizeFs(mFetchIndex, getCachePath, {
+  useCache({ fetch }) {
+    return !fetch
+  },
+  maxAge: MAX_AGE_MS,
+  serialization: 'none',
+})
--- src/main.js
+++ src/main.js
@@ -1,8 +1,3 @@
-import { env } from 'process'
-
-import getCacheDir from 'cachedir'
-
-import { moizeFs } from './cache/moize.js'
 import { fetchIndex } from './fetch.js'
 import { normalizeIndex } from './normalize.js'
 import { getOpts } from './options.js'
@@ -11,36 +6,11 @@ import { getOpts } from './options.js'
 // Versions are already sorted from newest to oldest.
 const allNodeVersions = async function (opts) {
   const { fetch, fetchNodeOpts } = getOpts(opts)
-  const content = await cFetchIndex({ fetchNodeOpts, fetch })
+  const content = await fetchIndex({ fetchNodeOpts, fetch })
   const index = JSON.parse(content)
   const versionsInfo = normalizeIndex(index)
   return versionsInfo
 }
-
-// The cache is persisted to `GLOBAL_CACHE_DIR/nve/versions.json`.
-const getCachePath = function () {
-  const cacheDir = getCacheDir(CACHE_DIR)
-  const cacheFilename = env.TEST_CACHE_FILENAME || CACHE_FILENAME
-  return `${cacheDir}/${cacheFilename}`
-}
-
-const CACHE_DIR = 'nve'
-const CACHE_FILENAME = 'node_versions.json'
-
-// One hour
-const MAX_AGE_MS = 36e5
-
-// Cache the return value on the filesystem.
-// It has a TTL of one hour.
-// If the `fetch` option is `true`, we do not read the cache (but still write
-// it)
-const cFetchIndex = moizeFs(fetchIndex, getCachePath, {
-  useCache({ fetch }) {
-    return !fetch
-  },
-  maxAge: MAX_AGE_MS,
-  serialization: 'none',
-})
 
 // We do not use `export default` because Babel transpiles it in a way that
 // requires CommonJS users to `require(...).default` instead of `require(...)`.
