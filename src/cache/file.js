551baf678966537ec8d248632748171f87622342
ehmicky <ehmicky@gmail.com>
2022-02-06 15:37:02 +0100

    Fix linting

--- src/cache/file.js
+++ src/cache/file.js
@@ -16,7 +16,7 @@ const CACHE_FILENAME = 'versions.json'
 
 // Retrieve cache file's content
 export const getCacheFileContent = async function (cacheFile) {
-  const cacheFileContent = await fs.readFile(cacheFile, 'utf8')
+  const cacheFileContent = await fs.readFile(cacheFile)
   const { lastUpdate, ...versionsInfo } = JSON.parse(cacheFileContent)
   const age = Date.now() - lastUpdate
   return { versionsInfo, age }
